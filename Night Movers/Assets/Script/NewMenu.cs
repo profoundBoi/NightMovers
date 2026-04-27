1b850402f622793a20e45e38ad6da75d46ad9350
profoundBoi <connor170103@gmail.com>
2026-04-27 15:13:13 +0200

    noooo

--- Night Movers/Assets/Script/NewMenu.cs
+++ Night Movers/Assets/Script/NewMenu.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI;
+using TMPro;
 
 public class NewMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("UI Buttons")]
+    public Button hostButton;
+    public Button joinButton;
+    public Button serverButton;
+
+    private void Start()
     {
-        
+        // Assign button listeners
+        hostButton.onClick.AddListener(StartHost);
+        joinButton.onClick.AddListener(StartClient);
+
+        if (serverButton != null)
+            serverButton.onClick.AddListener(StartServer);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartHost()
     {
-        
+        Debug.Log("Starting Host...");
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        Debug.Log("Starting Client...");
+        NetworkManager.Singleton.StartClient();
+    }
+
+    public void StartServer()
+    {
+        Debug.Log("Starting Server...");
+        NetworkManager.Singleton.StartServer();
     }
 }
